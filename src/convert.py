147942d6393b833dd1b029d2c849add584ff4335
Beatriz Galindo <beagalin@amazon.com>
2024-12-10 19:06:57 +0000

    Adding more test cases and reestructure of the code

--- src/convert.py
+++ src/convert.py
@@ -1,34 +1,21 @@
+# Words for the basic numbers
+ONES = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+TEENS = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+TENS = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+HUNDRED = "hundred"
+THOUSAND = "thousand"
+MILLION = "million"
+
 def number_to_words(number):
-    if number < 0 or number > 100000:
-        return "Number out of range. Please enter a number between 0 and 100,000."
-
-    # Words for the basic numbers
-    ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
-    tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-
-    # Special case for zero
+    # Handle specific case for 0
     if number == 0:
         return "zero"
 
-    # Handle numbers from 1 to 99999
+    # Handle specific case for 100,000
     if number == 100000:
         return "one hundred thousand"
 
-    # Helper function for numbers less than 1000
-    def handle_hundreds(n):
-        if n == 0:
-            return ""
-        elif n < 10:
-            return ones[n]
-        elif 10 < n < 20:
-            return teens[n - 10]
-        elif n < 100:
-            return tens[n // 10] + ('' if n % 10 == 0 else '-' + ones[n % 10])
-        else:
-            return ones[n // 100] + " hundred" + ('' if n % 100 == 0 else ' and ' + handle_hundreds(n % 100))
-
-    # Split the number into thousands and hundreds part
+    # Split number into thousands and hundreds parts
     thousands_part = number // 1000
     remainder_part = number % 1000
 
@@ -37,10 +24,22 @@ def number_to_words(number):
         words += handle_hundreds(thousands_part) + " thousand"
         if 0 < remainder_part < 100:
             words += " and "
-        elif 0< remainder_part < 1000:
+        elif 0 < remainder_part < 1000:
             words += ", "
 
     if remainder_part > 0:
         words += handle_hundreds(remainder_part)
 
     return words.strip()
+
+def handle_hundreds(n):
+    if n == 0:
+        return ""
+    elif n < 10:
+        return ONES[n]
+    elif 10 < n < 20:
+        return TEENS[n - 10]
+    elif n < 100:
+        return TENS[n // 10] + ('' if n % 10 == 0 else '-' + ONES[n % 10])
+    else:
+        return ONES[n // 100] + " hundred" + ('' if n % 100 == 0 else ' and ' + handle_hundreds(n % 100))
--- tests/test_convert.py
+++ tests/test_convert.py
@@ -13,6 +13,7 @@ class TestNumberToWords(unittest.TestCase):
     def test_teens(self):
         self.assertEqual(number_to_words(11), "eleven")
         self.assertEqual(number_to_words(15), "fifteen")
+        self.assertEqual(number_to_words(52), "fifty-two")
 
     def test_tens(self):
         self.assertEqual(number_to_words(20), "twenty")
@@ -21,11 +22,15 @@ class TestNumberToWords(unittest.TestCase):
     def test_hundreds(self):
         self.assertEqual(number_to_words(100), "one hundred")
         self.assertEqual(number_to_words(101), "one hundred and one")
+        self.assertEqual(number_to_words(115), "one hundred and fifteen")
+        self.assertEqual(number_to_words(352), "three hundred and fifty-two")
         self.assertEqual(number_to_words(999), "nine hundred and ninety-nine")
 
     def test_thousands(self):
         self.assertEqual(number_to_words(1000), "one thousand")
         self.assertEqual(number_to_words(12055), "twelve thousand and fifty-five")
+        self.assertEqual(number_to_words(12300), "twelve thousand, three hundred")
+        self.assertEqual(number_to_words(12345), "twelve thousand, three hundred and forty-five")
         self.assertEqual(number_to_words(99999), "ninety-nine thousand, nine hundred and ninety-nine")
 
     def test_exact_thousand(self):
