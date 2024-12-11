1d570daded660fc7eb204da8d98f969b9adfab9f
Beatriz Galindo <beagalin@amazon.com>
2024-12-11 15:39:44 +0000

    Create a class NumberToWordsConverter

--- src/convert.py
+++ src/convert.py
@@ -1,45 +1,62 @@
-# Words for the basic numbers
-ONES = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-TEENS = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
-TENS = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-HUNDRED = "hundred"
-THOUSAND = "thousand"
-MILLION = "million"
+class NumberToWordsConverter:
+    """
+    A class to convert numbers into their English word representation.
 
-def number_to_words(number):
-    # Handle specific case for 0
-    if number == 0:
-        return "zero"
+    Attributes:
+        number (int): The number to convert.
+    """
+    # Private constants for number-to-words mapping
+    _ONES = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    _TEENS = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    _TENS = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    _HUNDRED = "hundred"
+    _THOUSAND = "thousand"
+    _MILLION = "million"
 
-    # Handle specific case for 100,000
-    if number == 100000:
-        return "one hundred thousand"
+    def __init__(self,number):
+        self.number = number
 
-    # Split number into thousands and hundreds parts
-    thousands_part = number // 1000
-    remainder_part = number % 1000
+    def number_to_words(self):
+        """
+        Converts the number to its English word representation.
 
-    words = ""
-    if thousands_part > 0:
-        words += handle_hundreds(thousands_part) + " thousand"
-        if 0 < remainder_part < 100:
-            words += " and "
-        elif 0 < remainder_part < 1000:
-            words += ", "
+        Returns:
+            str: The English word representation of the number.
+        """
 
-    if remainder_part > 0:
-        words += handle_hundreds(remainder_part)
+        # Handle specific case for 0
+        if self.number == 0:
+            return "zero"
 
-    return words.strip()
+        # Handle specific case for 100,000
+        if self.number == 100000:
+            return "one hundred thousand"
 
-def handle_hundreds(n):
-    if n == 0:
-        return ""
-    elif n < 10:
-        return ONES[n]
-    elif 10 < n < 20:
-        return TEENS[n - 10]
-    elif n < 100:
-        return TENS[n // 10] + ('' if n % 10 == 0 else '-' + ONES[n % 10])
-    else:
-        return ONES[n // 100] + " hundred" + ('' if n % 100 == 0 else ' and ' + handle_hundreds(n % 100))
+        # Split number into thousands and hundreds parts
+        thousands_part = self.number // 1000
+        remainder_part = self.number % 1000
+
+        words = ""
+        if thousands_part > 0:
+            words += self.handle_hundreds(thousands_part) + " thousand"
+            if 0 < remainder_part < 100:
+                words += " and "
+            elif 0 < remainder_part < 1000:
+                words += ", "
+
+        if remainder_part > 0:
+            words += self.handle_hundreds(remainder_part)
+
+        return words.strip()
+
+    def handle_hundreds(self, n):
+        if n == 0:
+            return ""
+        elif n < 10:
+            return self._ONES[n]
+        elif 10 < n < 20:
+            return self._TEENS[n - 10]
+        elif n < 100:
+            return self._TENS[n // 10] + ('' if n % 10 == 0 else '-' + self._ONES[n % 10])
+        else:
+            return self._ONES[n // 100] + " hundred" + ('' if n % 100 == 0 else ' and ' + self.handle_hundreds(n % 100))
